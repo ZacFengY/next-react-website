4643e801e7db29b3f2047c3df17a6039ef96f006
yu.feng <yfeng2@ebay.com>
2021-07-23 18:55:31 +0800

    about me

--- constants.js
+++ constants.js
@@ -1,1 +1,19 @@
 export const MENUS = []
+
+export const friends = [
+  {
+    name: 'Blade',
+    link: '',
+    description: 'UI大佬',
+  },
+  {
+    name: '奥给',
+    link: '',
+    description: '炒股专家',
+  },
+  {
+    name: '小富贵',
+    link: '',
+    description: '开心果',
+  },
+]
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import { Row, Col, Card } from 'antd'
 import Image from 'next/image'
+import Label from '../components/Label'
+import { friends } from '../constants'
 
 export default function Home() {
   return (
@@ -44,7 +46,21 @@ export default function Home() {
             <Col sm={24} xs={24} md={16}>
               <Row gutter={[16, 16]} className='column'>
                 <Col sm={24} xs={24} md={24} className='about-me'>
-                  <Card title='About Me' className='about-card about-about-me'></Card>
+                  <Card title='About Me' className='about-card about-about-me'>
+                    <div className='img-wrap-100 city-sky'>
+                      <Label />
+                    </div>
+                    <div className='img-wrap-100 city'>
+                      <Image
+                        layout='responsive'
+                        src='/images/citybg.png'
+                        alt='bg'
+                        title='bg'
+                        width={220}
+                        height={50}
+                      />
+                    </div>
+                  </Card>
                 </Col>
                 <Col sm={24} xs={24} md={24} className='about-github-wechat'>
                   <Row gutter={[16, 16]}>
@@ -70,7 +86,19 @@ export default function Home() {
                       </Card>
                     </Col>
                     <Col sm={12} xs={12} md={12}>
-                      <Card title='Wechat' className='about-card about-wechat' />
+                      <Card title='Wechat' className='about-card about-wechat'>
+                        <div className='img-wrap-40'>
+                          <Image
+                            layout='responsive'
+                            src='/images/mywxcode.png'
+                            alt='myWechatCode'
+                            title='聊起来'
+                            width={400}
+                            height={400}
+                          />
+                        </div>
+                        <h3 className='img-detail'>zhongyugailea</h3>
+                      </Card>
                     </Col>
                   </Row>
                 </Col>
@@ -81,10 +109,32 @@ export default function Home() {
         <Col sm={24} xs={24} md={6}>
           <Row gutter={[16, 16]} className='column'>
             <Col sm={24} xs={24} md={24} className='where'>
-              <Card title='Where' className='about-card about-where' />
+              <Card title='Where' className='about-card about-where'>
+                <div className='img-wrap-60'>
+                  <Image
+                    layout='responsive'
+                    src='/images/myaddr.png'
+                    alt='myAddress'
+                    title='聊起来'
+                    width={451}
+                    height={143}
+                  />
+                </div>
+              </Card>
             </Col>
             <Col sm={24} xs={24} md={24} className='friends'>
-              <Card title='Friends' className='about-card about-friends' />
+              <Card title='Friends' className='about-card about-friends'>
+                <div className='friend-wrapper'>
+                  {friends.map(it => (
+                    <div className='pre-friend' key={it.name}>
+                      <span>{it.name}</span>
+                      <div>
+                        <a href={it.link}>{it.description}</a>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Card>
             </Col>
           </Row>
         </Col>
